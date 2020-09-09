9c73721901fa0718f49a8722cfeff908ce348773
wwiecek <20645798+wwiecek@users.noreply.github.com>
2020-09-09 14:17:20 +0100

    skip lengthy travis tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,10 +1,11 @@
 test_that('demandPriceTaker() works',{
   skip_on_cran()
+  skip_on_travis()
   population <- 31.99 # In millions
   gdp_pc <- 6.71 # In thousand dollars
   frac_high_risk <- 0.131
   loss2yr <- 0.269
-  parameters <- Parameters$new(population=population, gdp_pc=gdp_pc, 
+  parameters <- Parameters$new(population=population, gdp_pc=gdp_pc,
                                frac_high_risk=frac_high_risk, loss2yr=loss2yr)
   demand = demandPriceTaker(parameters)
   expect_equal(class(demand),'list')
@@ -13,7 +14,7 @@ test_that('demandPriceTaker() works',{
   expect_true(all(opt>=0))
   expect_is(demand$allCapacities,c("matrix", "array" ))
   expect_true(all(demand$allCapacities>=0))
-  demand = demandPriceTaker(population = population, gdp_pc = gdp_pc, 
+  demand = demandPriceTaker(population = population, gdp_pc = gdp_pc,
                             frac_high_risk = frac_high_risk, loss2yr = loss2yr)
   expect_equal(class(demand),'list')
   expect_length(demand,3)
@@ -25,11 +26,12 @@ test_that('demandPriceTaker() works',{
 
 test_that('portfolioPriceTaker() works',{
   skip_on_cran()
+  skip_on_travis()
   population <- 31.99 # In millions
   gdp_pc <- 6.71 # In thousand dollars
   frac_high_risk <- 0.131
   loss2yr <- 0.269
-  parameters <- Parameters$new(population=population, gdp_pc=gdp_pc, 
+  parameters <- Parameters$new(population=population, gdp_pc=gdp_pc,
                                frac_high_risk=frac_high_risk, loss2yr=loss2yr)
   port = portfolioPriceTaker(parameters,price = 10)
   expect_equal(class(port),'list')
@@ -39,7 +41,7 @@ test_that('portfolioPriceTaker() works',{
   expect_gte(port$cost,0)
   expect_gte(port$expBenefits,0)
   expect_gte(port$expCapacity,0)
-  port = portfolioPriceTaker(population=population, gdp_pc=gdp_pc, 
+  port = portfolioPriceTaker(population=population, gdp_pc=gdp_pc,
                              frac_high_risk=frac_high_risk, loss2yr=loss2yr,
                              steps = c(10),
                              price = 10,return_benefit_args = TRUE)
